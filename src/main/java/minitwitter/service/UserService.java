21e3f51e5ee6104206c9c3f5dacd95565d97046f
Rakshit Pithadia <rpithadia25@gmail.com>
2018-03-11 00:51:46 -0800

    Added more information to Userservice.

--- src/main/java/minitwitter/service/UserService.java
+++ src/main/java/minitwitter/service/UserService.java
@@ -9,12 +9,18 @@ import java.util.Map;
 public class UserService {
     Map<String, String> mapUserWthId = new HashMap();
 
-    // HardCoded credentials for now.
+    // Hardcoded credentials for now.
     public UserService() {
         mapUserWthId.put("batman", "batman");
         mapUserWthId.put("superman", "superman");
         mapUserWthId.put("catwoman", "catwoman");
         mapUserWthId.put("daredevil", "daredevil");
+        mapUserWthId.put("alfred", "alfred");
+        mapUserWthId.put("dococ", "dococ");
+        mapUserWthId.put("zod", "zod");
+        mapUserWthId.put("spiderman", "spiderman");
+        mapUserWthId.put("ironman", "ironman");
+        mapUserWthId.put("profx", "profx");
     }
 
     public String getUserNameByID(String userId) {
